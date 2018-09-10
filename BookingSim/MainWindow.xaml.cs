99170723a649b5e294e01758cc6427615c4b1600
cdavid11 <chaddavidson001@gmail.com>
2018-09-09 22:46:44 -0400

    Initial Commit for Booking Sim Project

---
+++ BookingSim/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookingSim
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            PerformGeneralApplicationSetup();
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Actions to perform before the visual loading of the application
+        /// </summary>
+        public void PerformGeneralApplicationSetup()
+        {
+
+        }
+
+        #region Button Clicks
+        /// <summary>
+        /// Opens a new NewGameWindow
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Opens a new LoadGameWindow 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadGame_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Opens a new EditModeWindow
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditMode_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Closes the main window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitGame_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        /// <summary>
+        /// Actions performed upon closing of this window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WindowClosing_Event(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show($"Why you no wanna book no more?", $"Click OK to Exit", MessageBoxButton.OK);
+        }
+    }
+
+
+}
---
+++ DataDLL/Wrestler.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDLL
+{
+    public class WrestlerType
+    {
+        #region Class Enums
+
+        /// <summary>
+        /// Denotes a Wrestler's Gender...just don't assume
+        /// </summary>
+        public enum Genders
+        {
+            Male = 0,
+            Female = 1,
+            Other = 2
+        }
+
+        /// <summary>
+        /// Denotes a Wrestler's Position within the company
+        /// </summary>
+        public enum Positions
+        {
+            Jobber = 0,
+            Opener = 1,
+            Midcard = 2,
+            UpperMidcard = 3,
+            MainEvent = 4
+        }
+
+        /// <summary>
+        /// Denotes a Wrestler's Disposition towards the crowd
+        /// </summary>
+        public enum Dispositions
+        {
+            Face = 0,
+            Tweener = 1,
+            Hell = 2
+        }
+
+        /// <summary>
+        /// Regions where workers originate
+        /// </summary>
+        public enum Regions
+        {
+            SouthEast = 0,
+            NorthEast = 1,
+            MidWest = 2,
+            SouthWest = 3,
+            NorthWest = 4,
+            EastCanada = 5,
+            CentralCanada = 6,
+            WesternCanada = 7,
+            NorthMexico = 8,
+            SouthMexico = 9
+        }
+        #endregion
+
+        private string _realName;
+        private string _wrestlerName;
+        private Genders _gender;
+        private Positions _position;
+        private Dispositions _dispostion;
+        private string _homeTown;
+        private Regions _region;
+
+        /// <summary>
+        /// The wrestler's real name
+        /// </summary>
+        public string RealName
+        {
+            get { return _realName; }
+            set
+            {
+                _realName = value;
+            }
+        }
+
+        /// <summary>
+        /// The wrestler's work name
+        /// </summary>
+        public string Wrestlername
+        {
+            get { return _wrestlerName; }
+            set
+            {
+                _wrestlerName = value;
+            }
+        }
+
+        /// <summary>
+        /// A wrestler's gender
+        /// </summary>
+        public Genders Gender
+        {
+            get { return _gender; }
+            set
+            {
+                _gender = value;
+            }
+        }
+
+        /// <summary>
+        /// A wrestler's position within the company
+        /// </summary>
+        public Positions Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+            }
+        }
+
+        /// <summary>
+        /// A wrestler's dispostion towards the crowd
+        /// </summary>
+        public Dispositions Disposition
+        {
+            get { return _dispostion; }
+            set
+            {
+                _dispostion = value;
+            }
+        }
+
+        /// <summary>
+        /// A wrestler's hometown
+        /// </summary>
+        public string HomeTown
+        {
+            get { return _homeTown; }
+            set
+            {
+                _homeTown = value;
+            }
+        }
+
+        public Regions Region
+        {
+            get { return _region; }
+            set
+            {
+                _region = value;
+            }
+        }
+    }
+
+    public class WrestlerData
+    {
+
+    }
+}
