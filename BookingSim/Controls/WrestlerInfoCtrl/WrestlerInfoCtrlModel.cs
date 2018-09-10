4c9a2cbaefed0afeb02e2f81970c3440fc2b4fd2
cdavid11 <chaddavidson001@gmail.com>
2018-09-09 23:48:11 -0400

    No current build but trying to link dlls

--- BookingSim/Controls/WrestlerInfoCtrl/WrestlerInfoCtrlModel.cs
+++ BookingSim/Controls/WrestlerInfoCtrl/WrestlerInfoCtrlModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using DataDLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BookingSim.Controls.WrestlerInfoCtrl
+namespace BookingSim.Controls
 {
     class WrestlerInfoCtrlModel
     {
+        private WrestlerType _wrestler;
+
+        public WrestlerType Wrestler
+        {
+            get { return Wrestler; }
+            set
+            {
+                _wrestler = value;
+            }
+        }
     }
 }
